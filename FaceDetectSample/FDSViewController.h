752404864b94a7deeadfa394bdbb25b33d136b80
Naoto Kaneko <kaneko@keyvalue.jp>
2013-08-08 14:43:57 +0900

    ipadで顔検出してオブジェクトを表示する
    iPhoneは画像の向きを正しくとれない

--- FaceDetectSample/FDSViewController.h
+++ FaceDetectSample/FDSViewController.h
@@ -9,6 +9,17 @@
 #import <UIKit/UIKit.h>
 #import <AVFoundation/AVFoundation.h>
 
+enum {
+    PHOTOS_EXIF_0ROW_TOP_0COL_LEFT = 1,
+    PHOTOS_EXIF_0ROW_TOP_0COL_RIGHT = 2,
+    PHOTOS_EXIF_0ROW_BOTTOM_0COL_RIGHT = 3,
+    PHOTOS_EXIF_0ROW_BOTTOM_0COL_LEFT = 4,
+    PHOTOS_EXIF_0ROW_LEFT_0COL_TOP = 5,
+    PHOTOS_EXIF_0ROW_RIGHT_0COL_TOP = 6,
+    PHOTOS_EXIF_0ROW_RIGHT_0COL_BOTTOM = 7,
+    PHOTOS_EXIF_0ROW_LEFT_0COL_BOTTOM = 8
+};
+
 @interface FDSViewController : UIViewController <AVCaptureVideoDataOutputSampleBufferDelegate>
 
 @end
--- FaceDetectSample/FDSViewController.m
+++ FaceDetectSample/FDSViewController.m
@@ -7,13 +7,16 @@
 //
 
 #import "FDSViewController.h"
+#import <CoreImage/CoreImage.h>
 #import <CoreMedia/CoreMedia.h>
 #import <CoreVideo/CoreVideo.h>
+#import <QuartzCore/QuartzCore.h>
 
 @interface FDSViewController ()
 
 @property (nonatomic) AVCaptureSession *session;
 @property (nonatomic) AVCaptureVideoPreviewLayer *previewLayer;
+@property (nonatomic) CIDetector *faceDetector;
 
 @end
 
@@ -23,6 +26,22 @@
 {
     [super viewDidLoad];
     
+    // 顔検出器を初期化する
+    NSDictionary *detectorOptions = @{CIDetectorAccuracy : CIDetectorAccuracyLow};
+    self.faceDetector = [CIDetector detectorOfType:CIDetectorTypeFace context:nil options:detectorOptions];
+    
+    [self setupAVCapture];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+#pragma mark - Local methods
+
+- (void)setupAVCapture
+{
     // セッションを初期化する
     self.session = [[AVCaptureSession alloc] init];
     
@@ -52,35 +71,38 @@
     
     // ビデオデータとして出力するように設定する
     AVCaptureVideoDataOutput *videoDataOutput = [[AVCaptureVideoDataOutput alloc] init];
-    videoDataOutput.videoSettings = @{(id)kCVPixelBufferPixelFormatTypeKey : [NSNumber numberWithInt:kCVPixelFormatType_32BGRA]}; // 出力フォーマットを指定する
-    videoDataOutput.alwaysDiscardsLateVideoFrames = YES; // 処理に時間がかかるフレームを破棄する
-    [videoDataOutput setSampleBufferDelegate:self queue:dispatch_queue_create("captureOutputQueue", NULL)]; // フレームを処理するデリゲート先とキューを指定する
+    
+    // 出力フォーマットを指定する
+    videoDataOutput.videoSettings = @{(id)kCVPixelBufferPixelFormatTypeKey : [NSNumber numberWithInt:kCVPixelFormatType_32BGRA]};
+    
+    // 処理に時間がかかるフレームを破棄する
+    videoDataOutput.alwaysDiscardsLateVideoFrames = YES;
+    
+    // フレームを処理するデリゲート先とキューを指定する
+    [videoDataOutput setSampleBufferDelegate:self queue:dispatch_queue_create("captureOutputQueue", NULL)];
     if ([self.session canAddOutput:videoDataOutput]) {
         [self.session addOutput:videoDataOutput];
     }
-    
-    // 1秒間あたり15回画像をキャプチャする
-    AVCaptureConnection *videoConnection = [videoDataOutput connectionWithMediaType:AVMediaTypeVideo];
-    videoConnection.videoMinFrameDuration = CMTimeMake(1, 15);
     
     // プレビューを表示するレイヤーを初期化する
     self.previewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:self.session];
     CGSize rotatedSize = [self rotatedViewSize];
     self.previewLayer.frame = CGRectMake(0, 0, rotatedSize.width, rotatedSize.height);
-    self.previewLayer.videoGravity = AVLayerVideoGravityResizeAspect; // アスペクト比を維持しつつ画面いっぱいに表示する
-    self.previewLayer.connection.videoOrientation = [[UIApplication sharedApplication] statusBarOrientation]; // プレビューの向きをデバイスの向きに合わせる
+    
+    // アスペクト比を維持しつつ画面いっぱいに表示する
+    self.previewLayer.videoGravity = AVLayerVideoGravityResizeAspect;
+    
+    // プレビューの向きをデバイスの向きに合わせる
+    self.previewLayer.connection.videoOrientation = [[UIApplication sharedApplication] statusBarOrientation];
+    
+    // 1秒間あたり15回画像をキャプチャする
+    self.previewLayer.connection.videoMinFrameDuration = CMTimeMake(1, 15);
+    
     [self.view.layer addSublayer:self.previewLayer];
     
     // 入出力を開始する
     [self.session startRunning];
 }
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-}
-
-#pragma mark - Local methods
 
 - (CGSize)rotatedViewSize
 {
@@ -94,11 +116,87 @@
     return isPortrait ? CGSizeMake(min, max) : CGSizeMake(max, min);
 }
 
+- (void)drawRectOnLayer:(CALayer *)layer center:(CGPoint)center
+{
+    CGFloat width = 30;
+    CGFloat height = 30;
+    
+    CALayer *sublayer = [CALayer layer];
+    sublayer.frame = CGRectMake(center.x - width / 2, center.y - height / 2, width, height);
+    sublayer.backgroundColor = [[UIColor clearColor] CGColor];
+    sublayer.borderColor = [[UIColor redColor] CGColor];
+    sublayer.borderWidth = 2.0;
+    
+    // 削除するときのために名前をつける
+    sublayer.name = @"FaceLayer";
+    
+    [layer addSublayer:sublayer];
+}
+
 #pragma mark - AVCaptureVideoDataOutputSampleBufferDelegate
 
 // キャプチャしたフレームを処理する
 - (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection
 {
+    // CIImageを取得する
+    CVPixelBufferRef pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
+    CFDictionaryRef attachments = CMCopyDictionaryOfAttachments(kCFAllocatorDefault, sampleBuffer, kCMAttachmentMode_ShouldPropagate);
+    CIImage *ciImage = [[CIImage alloc] initWithCVPixelBuffer:pixelBuffer options:(__bridge NSDictionary *)attachments];
+    
+    if (attachments) {
+        CFRelease(attachments);
+    }
+    
+    // 検出する際の画像の向きを指定する
+    int exifOrientation = UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone ? 5 : 3;
+    NSDictionary *imageOptions = @{CIDetectorImageOrientation : [NSNumber numberWithInt:exifOrientation]};
+    
+    // 顔検出を実行する
+    NSArray *features = [self.faceDetector featuresInImage:ciImage options:imageOptions];
+    
+    if (features.count == 0) {
+        NSLog(@"-");
+        
+        dispatch_async(dispatch_get_main_queue(), ^(void) {
+            // 以前に描画された図形を消去する
+            NSArray *sublayers = [NSArray arrayWithArray:self.previewLayer.sublayers];
+            for (CALayer *layer in sublayers) {
+                if ([layer.name isEqualToString:@"FaceLayer"]) {
+                    [layer removeFromSuperlayer];
+                }
+            }
+        });
+        
+        return;
+    } else {
+        NSLog(@"Face detected");
+    }
+    
+    // UIはメインスレッドで更新する
+    dispatch_async(dispatch_get_main_queue(), ^(void) {
+        // 以前に描画された図形を消去する
+        NSArray *sublayers = [NSArray arrayWithArray:self.previewLayer.sublayers];
+        for (CALayer *layer in sublayers) {
+            if ([layer.name isEqualToString:@"FaceLayer"]) {
+                [layer removeFromSuperlayer];
+            }
+        }
+        
+        // 検出された顔の上に図形を描画する
+        for (CIFaceFeature *feature in features) {
+            if ([feature hasLeftEyePosition]) {
+                [self drawRectOnLayer:self.previewLayer center:feature.leftEyePosition];
+            }
+            
+            if ([feature hasRightEyePosition]) {
+                [self drawRectOnLayer:self.previewLayer center:feature.rightEyePosition];
+            }
+            
+            if ([feature hasMouthPosition]) {
+                [self drawRectOnLayer:self.previewLayer center:feature.mouthPosition];
+            }
+        }
+    });
 }
 
 @end
